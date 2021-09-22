e7442f9a153f3ef7858151dfde0e56f3bf7c5308
Mansour <mansurjavadi@gmail.com>
2021-09-22 21:55:52 +0330

    Remove extra codes

--- lib/installprompt.dart
+++ lib/installprompt.dart
@@ -5,11 +5,6 @@ import 'package:flutter/services.dart';
 class InstallPrompt {
   static const MethodChannel _channel = const MethodChannel('installprompt');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
   static void showInstallPrompt({String? referrer}) {
     _channel.invokeMethod('showInstallPrompt', {"referrer": referrer ?? null});
   }
